968b2bc530a4e0fc8bb04c6f24a4d8ef46bceb28
Vishnu tyagi <106895072+Vishnutyagi@users.noreply.github.com>
2024-04-17 16:05:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+() => { 
+    cy.visit(baseUrl + "/main.html"); 
+    cy.get('#projects').children().should('have.length.greaterThan', 0); 
+    cy.get('#projects').find('ul').children().should('have.length.greaterThan', 0); 
+    cy.get('#projects').find('ul').children().each(($li) => { 
+        cy.wrap($li).invoke('text').then((text) => { 
+            expect(text.trim()).to.have.length.greaterThan(0); 
+        }); 
+    }); 
+}
